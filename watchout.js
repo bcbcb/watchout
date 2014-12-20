2141d4abd5cb311dc09424ce705d10e2ceffca0b
bcbcb <bcbcb@users.noreply.github.com>
2014-12-19 17:44:19 -0800

    got stuff to move around

--- watchout.js
+++ watchout.js
@@ -1,2 +1,41 @@
-// start slingin' some d3 here.
+var svg = d3.select('body').append('svg');
 
+var numberOfAsteroids = 2;
+var asteroidsRange = [];
+var asteroidData;
+
+for (var i = 0; i < numberOfAsteroids; i++) {
+  asteroidsRange[i] = i;
+}
+
+
+var randomX = function () {
+  return Math.random() * 600;
+}
+var randomY = function () {
+  return Math.random() * 500;
+}
+
+var newPositions = function() {
+  return asteroidsRange.map(function(i) {
+    return {
+      id: i,
+      x: randomX(),
+      y: randomY()
+    };
+  });
+}
+
+asteroidData = newPositions();
+
+var asteroids = svg.selectAll('circle')
+  .data(asteroidData)
+  .enter()
+  .append('circle')
+  .attr('cx', function(d) { return d.x })
+  .attr('cy', function(d) { return d.y })
+  .attr('r', 10);
+
+var move = function () {
+
+}
